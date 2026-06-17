221723e70ae625134380eab5f3a5482dde20ce79
salomiz123 <salomizrahi85@gmail.com>
2026-06-17 15:58:51 -0300

    no termine
    n

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import { preguntar } from './lib/consola.ts';
 
 function repetirLetra(letra: string, cantidad: number): string {
-  // COMPLETAR
+let vecesrepetido=tamaño
+  while (vecesrepetido >cantidad)
+
 }
 
 let caracter: string = preguntar('¿Con que caracter vas a dibujar el cuadrado? ');
 let tamaño: number = Number(preguntar('¿De qué tamaño? '));
 
-// COMPLETAR
+while (tamaño>0) {
+console.log(caracter)
+tamaño= tamaño-1
+}
