41e89b2c5acd3c99e68420bb2e21588b2624ec01
salomiz123 <salomizrahi85@gmail.com>
2026-07-01 13:11:50 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
 import { preguntar } from './lib/consola.ts';
 
 function repetirLetra(letra: string, cantidad: number): string {
-let vecesrepetido=tamaño
-  while (vecesrepetido >cantidad)
+  var i:number = 0
+  let acumulado:string= ""
+  while (i<cantidad){
+    acumulado= acumulado+letra
+    i=i+1
+  }
+  return acumulado
 
 }
+
 
 let caracter: string = preguntar('¿Con que caracter vas a dibujar el cuadrado? ');
 let tamaño: number = Number(preguntar('¿De qué tamaño? '));
 
+
+let fila:string= repetirLetra(caracter, tamaño)
 while (tamaño>0) {
-console.log(caracter)
-tamaño= tamaño-1
-}
+  console.log(fila)
+  tamaño= tamaño-1
+  }
+
